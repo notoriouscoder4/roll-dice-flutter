e50bf277995ea2428e41797a06d355108fc60f04
Sawankumar <notoriouscoder4@gmail.com>
2023-10-19 11:02:47 +0530

    Initial commit for roll dice

---
+++ lib/gradient_container.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+import 'package:rolldice/dice_roller.dart';
+
+const startAlignment = Alignment.topLeft;
+const endAlignment = Alignment.bottomRight;
+
+class GradientContainer extends StatelessWidget {
+  const GradientContainer(this.color1, this.color2, {super.key});
+
+  final Color color1, color2;
+
+  @override
+  Widget build(context) {
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: [color1, color2],
+          begin: startAlignment,
+          end: endAlignment,
+        ),
+      ),
+      child: const Center(
+        child: DiceRoller(),
+      ),
+    );
+  }
+}
+
+// const startAlignment = Alignment.topLeft;
+// const endAlignment = Alignment.bottomRight;
+//
+// class GradientContainer extends StatelessWidget {
+//   final List<Color> colors;
+//
+//   const GradientContainer({super.key, required this.colors});
+//
+//   @override
+//   Widget build(context) {
+//     return Container(
+//       decoration: BoxDecoration(
+//         gradient: LinearGradient(
+//           colors: colors,
+//           begin: startAlignment,
+//           end: endAlignment,
+//         ),
+//       ),
+//       child: const Center(
+//         child: StyledText('Hello World!'),
+//       ),
+//     );
+//   }
+// }
+
+// class GradientContainer extends StatelessWidget {
+//   const GradientContainer({super.key});
+//
+//   @override
+//   Widget build(context) {
+//     return Container(
+//       decoration: const BoxDecoration(
+//         gradient: LinearGradient(
+//           colors: [
+//             Color.fromARGB(255, 26, 2, 80),
+//             Color.fromARGB(255, 45, 7, 98),
+//           ],
+//           begin: startAlignment,
+//           end: endAlignment,
+//         ),
+//       ),
+//       child: const Center(
+//         child: StyledText('Hello World!'),
+//       ),
+//     );
+//   }
+// }
